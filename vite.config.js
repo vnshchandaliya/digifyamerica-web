831e11bd0b6d7ac63abc3fd4488f676c3d08c22b
rollpefedev1 <dev1@rollpe.com>
2025-08-01 23:52:00 +0530

    ok

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/digifyamerica-web/',
-  plugins: [react(), tailwindcss()],
+  base: '/digifyamerica-web/', // 👈 very important for GitHub Pages
+  plugins: [react(), tailwindcss()]
 })
